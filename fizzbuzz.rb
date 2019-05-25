e76854bc739c00dc4aee866fe3d634cb791827dd
Anthony Molina <antonio-molina@att.net>
2019-05-25 02:58:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,8 @@
 def fizzbuzz(int)
 if int 3 == 3 if the number is diviisible by 3
   puts "fizz"
-  elsif 5 == 5 if the number is divisible by 5
+elsif 5 == 5 if the number is divisible by 5
   puts "Buzz"
+elsif 15 == 15 if the number is divisible by 3 or 15
+  puts "Fizzbuzz"
   
